6dbfd06771ac24c3e3ff79d10a47bc8ece68f532
Anthony Bonta <zthun.coding@gmail.com>
2023-09-02 15:13:24 -0500

    chore: exports

--- packages/lumberjacky-log/src/index.ts
+++ packages/lumberjacky-log/src/index.ts
@@ -1,1 +1,5 @@
-export const Placeholder = 0;
+export * from './log-entry/log-entry';
+export * from './logger/logger';
+export * from './logger/logger-composite';
+export * from './logger/logger-console';
+export * from './logger/logger-silent';
