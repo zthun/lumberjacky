bd781510e1ffae79efce2499bf0e6de7f47bef46
Anthony Bonta <zthun.coding@gmail.com>
2023-09-09 16:25:18 -0500

    fix: export the context logger

--- packages/lumberjacky-log/src/index.ts
+++ packages/lumberjacky-log/src/index.ts
@@ -2,4 +2,5 @@ export * from './log-entry/log-entry';
 export * from './logger/logger';
 export * from './logger/logger-composite';
 export * from './logger/logger-console';
+export * from './logger/logger-context';
 export * from './logger/logger-silent';
