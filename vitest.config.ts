ba566778561c6b52933c199e8c4ec72867f46c18
Anthony Bonta <zthun.coding@gmail.com>
2024-07-22 14:59:59 -0500

    feat!: convert lumberjacky packages to esm modules

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,9 @@
-import tsConfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [tsConfigPaths()],
   test: {
-    environment: 'node',
-    testTimeout: 30000,
     coverage: {
+      all: false,
       provider: 'istanbul'
     }
   }
