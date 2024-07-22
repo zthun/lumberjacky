1933754aa27b3e0de5fa8203c14b27683a17ec45
Anthony Bonta <zthun.coding@gmail.com>
2024-07-22 18:09:24 -0500

    refactor: lodash to lodash-es

--- packages/lumberjacky-log/src/logger/logger-console.spec.ts
+++ packages/lumberjacky-log/src/logger/logger-console.spec.ts
@@ -1,4 +1,4 @@
-import { noop } from 'lodash';
+import { noop } from 'lodash-es';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { ZLogEntryBuilder } from '../log-entry/log-entry.mjs';
 import { ZLoggerConsole } from './logger-console.mjs';
